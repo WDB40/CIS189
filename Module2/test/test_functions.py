83db8093932a13aa55529d4b0c3a80ff95705fe5
WDB40 <wbrown1640@gmail.com>
2019-09-02 15:22:19 -0500

    Updating test names and adding additional

--- Module2/test/test_functions.py
+++ Module2/test/test_functions.py
@@ -13,11 +13,17 @@ from Module2.src.camper_age_input import convert_to_months
 
 
 class MyTestCase(unittest.TestCase):
-    def test_5_years(self):
+    def test_60_months(self):
         self.assertEqual(5, convert_to_months(60))
 
-    def test_10_years(self):
+    def test_54_months(self):
         self.assertEqual(4.5, convert_to_months(54))
+
+    def test_3_months(self):
+        self.assertEqual(.25, convert_to_months(3))
+
+    def test_72_months(self):
+        self.assertEqual(6, convert_to_months(72))
 
 
 if __name__ == '__main__':
