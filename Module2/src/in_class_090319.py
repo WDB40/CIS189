5c390bfc353d24fa61a8a1dcd16c4d039279afd7
WDB40 <wbrown1640@gmail.com>
2019-09-09 20:23:06 -0500

    Push 1 - Basic Input and Format Output Assignment

---
+++ Module2/src/in_class_090319.py
@@ -0,0 +1,15 @@
+"""
+Name: Wes Brown
+Assignment: Arithmetic Operations
+Date: 09/03/19
+
+Purpose: Show use of arithmetic operations
+"""
+
+
+def addition(x, y):
+    return x + y
+
+
+if __name__ == '__main__':
+    print(addition(1,2))
