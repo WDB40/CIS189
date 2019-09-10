9455372f004c990d80e8b1a7569d143ee7315373
WDB40 <wbrown1640@gmail.com>
2019-09-09 20:30:22 -0500

    Push 2 - Basic Input and Format Output Assignment

--- Module3/src/average_scores.py
+++ Module3/src/average_scores.py
@@ -1,2 +1,6 @@
 def average():
-    pass
+    val1 = input("Enter the first number: ")
+    val2 = input("Enter the second number: ")
+    val3 = input("Enter the third number: ")
+
+    return (val1 + val2 + val3) / 3
