60dd1df90fdc89cb544f0bdd2b3d66f6a072fd01
WDB40 <wbrown1640@gmail.com>
2019-09-16 21:27:29 -0500

    Raising exception for invalid third value.

--- Module4/src/validation_with_try.py
+++ Module4/src/validation_with_try.py
@@ -10,7 +10,7 @@ Purpose: To average 3 scores with exceptions.
 def average(score1, score2, score3):
     NUMBER_TESTS = 3
 
-    if score1 < 0 or score2 < 0:
+    if score1 < 0 or score2 < 0 or score3 < 0:
         raise ValueError
     else:
         average_score = float((score1 + score2 + score3) / NUMBER_TESTS)
