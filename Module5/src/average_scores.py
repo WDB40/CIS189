e61f21a1231530faf672f2d6a1d6d2a17fbabca2
WDB40 <wbrown1640@gmail.com>
2019-09-17 18:49:25 -0500

    Added more tests and completed average.

--- Module5/src/average_scores.py
+++ Module5/src/average_scores.py
@@ -1,2 +1,7 @@
 def average(score_list):
-    pass
+    total = 0
+
+    for value in score_list:
+        total = total + value
+
+    return total / len(score_list)
--- Module5/test/test_average_scores.py
+++ Module5/test/test_average_scores.py
@@ -7,6 +7,15 @@ class MyTestCase(unittest.TestCase):
     def test_average_2_list(self):
         self.assertEqual(25, average([20, 30]))
 
+    def test_average_3_list(self):
+        self.assertEqual(25, average([20, 25, 30]))
+
+    def test_average_4_list(self):
+        self.assertEqual(30, average([20, 25, 35, 40]))
+
+    def test_average_5_list(self):
+        self.assertEqual(25, average([15, 20, 25, 30, 35]))
+
 
 if __name__ == '__main__':
     unittest.main()
