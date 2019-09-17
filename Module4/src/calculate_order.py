5d07a883e921116fcbf246f108dd7e70b94c6400
WDB40 <wbrown1640@gmail.com>
2019-09-16 19:36:46 -0500

    Defining test cases.

---
+++ Module4/src/calculate_order.py
@@ -0,0 +1,10 @@
+def calculate_tax(amount):
+    pass
+
+
+def calculate_shipping(amount):
+    pass
+
+
+def calculate_order(price, cash_coupon, percent_coupon):
+    pass
---
+++ Module4/test/test_calculate_order.py
@@ -0,0 +1,52 @@
+import unittest
+
+
+class MyTestCase(unittest.TestCase):
+    def test_under_10_shipping(self):
+        pass
+
+    def test_between_10_30_shipping(self):
+        pass
+
+    def test_between_30_50_shipping(self):
+        pass
+
+    def test_over_50_shipping(self):
+        pass
+
+    def test_tax_5(self):
+        pass
+
+    def test_tax_15(self):
+        pass
+
+    def test_tax_30(self):
+        pass
+
+    def test_tax_45(self):
+        pass
+
+    def test_tax_60(self):
+        pass
+
+    def test_with_only_cash_coupon(self):
+        pass
+
+    def test_with_only_percent_coupon(self):
+        pass
+
+    def test_with_both_coupons(self):
+        pass
+
+    def test_total_order_1(self):
+        pass
+
+    def test_total_order_2(self):
+        pass
+
+    def test_total_order_3(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
