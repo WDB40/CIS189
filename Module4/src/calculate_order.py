d93a8a2dc6e3f361ebed41055c378ad435122228
WDB40 <wbrown1640@gmail.com>
2019-09-16 20:19:12 -0500

    Completing calculate_tax

--- Module4/src/calculate_order.py
+++ Module4/src/calculate_order.py
@@ -1,18 +1,21 @@
 def calculate_tax(amount):
-    pass
+    tax_amount = 0.06;
+    amount = float(amount)
+    return amount * tax_amount
 
 
 def calculate_shipping(amount):
-    shipping_cost = -1
-    amount = int(amount)
+    amount = float(amount)
 
-    if amount < 10:
+    if amount < 0:
+        shipping_cost = -1
+    elif amount < 10:
         shipping_cost = 5.95
     elif amount < 30:
         shipping_cost = 7.95
     elif amount < 50:
         shipping_cost = 11.95
-    elif shipping_cost >= 50:
+    else:
         shipping_cost = 0
 
     return shipping_cost
--- Module4/test/test_calculate_order.py
+++ Module4/test/test_calculate_order.py
@@ -5,40 +5,40 @@ from Module4.src.calculate_order import calculate_shipping, calculate_tax
 
 class MyTestCase(unittest.TestCase):
     def test_under_10_shipping(self):
-        self.assertEqual(calculate_shipping(5), 5.95)
+        self.assertEqual(5.95, calculate_shipping(5))
 
     def test_10_up_to_30_shipping(self):
-        self.assertEqual(calculate_shipping(10), 7.95)
+        self.assertEqual(7.95, calculate_shipping(10))
 
     def test_between_10_30_shipping(self):
-        self.assertEqual(calculate_shipping(20), 7.95)
+        self.assertEqual(7.95, calculate_shipping(20))
 
     def test_30_up_to_50_shipping(self):
-        self.assertEqual(calculate_shipping(30), 11.95)
+        self.assertEqual(11.95, calculate_shipping(30))
 
     def test_between_30_50_shipping(self):
-        self.assertEqual(calculate_shipping(40), 11.95)
+        self.assertEqual(11.95, calculate_shipping(40))
 
     def test_50_over_shipping(self):
-        self.assertEqual(calculate_shipping(50), 0)
+        self.assertEqual(0, calculate_shipping(50))
 
     def test_invalid_shipping(self):
-        self.assertEqual(calculate_shipping(-50), -1)
+        self.assertEqual(-1, calculate_shipping(-50))
 
     def test_tax_5(self):
-        self.assertEqual(calculate_tax(5), 0.3)
+        self.assertAlmostEqual(0.3, calculate_tax(5))
 
     def test_tax_15(self):
-        self.assertEqual(calculate_tax(15), 0.9)
+        self.assertAlmostEqual(0.9, calculate_tax(15))
 
     def test_tax_30(self):
-        self.assertEqual(calculate_tax(30), 1.8)
+        self.assertAlmostEqual(1.8, calculate_tax(30))
 
     def test_tax_45(self):
-        self.assertEqual(calculate_tax(45), 2.7)
+        self.assertAlmostEqual(2.7, calculate_tax(45))
 
     def test_tax_60(self):
-        self.assertEqual(calculate_tax(60), 3.6)
+        self.assertAlmostEqual(3.6, calculate_tax(60))
 
     def test_with_only_cash_coupon(self):
         pass
