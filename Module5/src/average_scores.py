883a5560453662c256984c087e326dcce19b6172
WDB40 <wbrown1640@gmail.com>
2019-09-17 19:15:57 -0500

    Completing the main method.

--- Module5/src/average_scores.py
+++ Module5/src/average_scores.py
@@ -1,7 +1,48 @@
+"""
+Program: average_scores.py
+Author: Wes Brown
+Last date modified: 09/17/19
+
+Purpose: Calculate the average from the list of values instead of passing each
+"""
+
+
 def average(score_list):
     total = 0
+    num_values = len(score_list)
+    avg_score = -1
 
-    for value in score_list:
-        total = total + value
+    if num_values != 0:
+        for value in score_list:
+            total = total + value
+        avg_score = total / len(score_list)
+    else:
+        avg_score = 0
 
-    return total / len(score_list)
+    return avg_score
+
+
+def get_test_scores():
+    INVALID_INPUT = -2
+    STOP = -1
+    user_input = 0
+    scores = []
+
+    while user_input != STOP:
+        try:
+            user_input = float(input("Enter a score (enter -1 to exit): "))
+        except ValueError:
+            user_input = INVALID_INPUT
+
+        if user_input != STOP and user_input != INVALID_INPUT:
+            scores.append(user_input)
+
+    return scores
+
+
+if __name__ == '__main__':
+    last_name = input("Enter the last name: ")
+    first_name = input("Enter the first name: ")
+    avg_scores = average(get_test_scores())
+
+    print("%s, %s - %.2f" % (last_name, first_name, avg_scores))
--- Module5/test/test_average_scores.py
+++ Module5/test/test_average_scores.py
@@ -1,3 +1,12 @@
+"""
+Program: test_average_scores.py (average_scores.py)
+Author: Wes Brown
+Last date modified: 09/17/19
+
+Purpose: To test the calculation of the average from the list of values instead of passing each
+"""
+
+
 import unittest
 
 from Module5.src.average_scores import average
@@ -15,6 +24,9 @@ class MyTestCase(unittest.TestCase):
 
     def test_average_5_list(self):
         self.assertEqual(25, average([15, 20, 25, 30, 35]))
+
+    def test_average_0_list(self):
+        self.assertEqual(0, average([]))
 
 
 if __name__ == '__main__':
