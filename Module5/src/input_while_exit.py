17d870f09a2c06f78afd0dd73b0f81c11f4facca
WDB40 <wbrown1640@gmail.com>
2019-09-23 21:12:01 -0500

    Copying file over.

---
+++ Module5/src/input_while_exit.py
@@ -0,0 +1,23 @@
+def get_list_of_numbers():
+    MIN = 1
+    MAX = 100
+    STOP = -1
+    user_input = 0
+    numbers = []
+
+    while user_input != STOP:
+        user_input = float(input("Enter a value between 1 and 100 (or -1 to exit): "))
+
+        if user_input != STOP and (MIN <= user_input <= MAX):
+            numbers.append(user_input)
+
+    return numbers
+
+
+def print_list(input_list):
+    for item in input_list:
+        print(item)
+
+
+if __name__ == '__main__':
+    print_list(get_list_of_numbers())
