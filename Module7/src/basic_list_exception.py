20d9a85d11d8a28f0ae248d0ca1fc42e9da53bfe
WDB40 <wbrown1640@gmail.com>
2019-10-01 18:45:40 -0500

    Setting up files for Basic List Exception Assignment.

---
+++ Module7/src/basic_list_exception.py
@@ -0,0 +1,36 @@
+"""
+Program: basic_list_exception.py
+Author: Wes Brown
+Last date modified: 10/01/19
+
+Purpose: Working with exceptions with lists in python
+"""
+
+
+def get_input():
+    INVALID_INPUT = -1
+    user_input = INVALID_INPUT
+
+    while user_input == INVALID_INPUT:
+        user_input = input("Enter a integer value: ")
+
+        try:
+            user_input = int(user_input)
+        except ValueError:
+            user_input = INVALID_INPUT
+
+    return user_input
+
+
+def make_list():
+    value_list = []
+
+    for i in range(0, 3):
+        value_list.insert(i, get_input())
+
+    return value_list
+
+
+if __name__ == '__main__':
+    user_input = get_input()
+    my_list = make_list()
--- Module7/test/test_basic_list_exception.py
+++ Module7/test/test_basic_list_exception.py
@@ -1,9 +1,9 @@
 """
-Program: test_basic_list.py (basic_list.py)
+Program: test_basic_list_exception.py (basic_list_exception.py)
 Author: Wes Brown
 Last date modified: 10/01/19
 
-Purpose: Test how we manipulate lists with python
+Purpose: Test working with exceptions with lists in python
 """
 
 
@@ -13,15 +13,15 @@ from Module7.src.basic_list import *
 
 
 class MyTestCase(unittest.TestCase):
-    @patch('Module7.src.basic_list.get_input', return_value=10)
+    @patch('Module7.src.basic_list_exception.get_input', return_value=10)
     def test_make_list(self, input):
         self.assertEqual(make_list(), [10, 10, 10])
 
-    @patch('Module7.src.basic_list.get_input', return_value=15)
+    @patch('Module7.src.basic_list_exception.get_input', return_value=15)
     def test_make_list_2(self, input):
         self.assertEqual(make_list(), [15, 15, 15])
 
-    @patch('Module7.src.basic_list.get_input', return_value=20)
+    @patch('Module7.src.basic_list_exception.get_input', return_value=20)
     def test_make_list_3(self, input):
         self.assertEqual(make_list(), [20, 20, 20])
 
