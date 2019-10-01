974c25da61b7af3a675b1c8ce59bc83e56cabba2
WDB40 <wbrown1640@gmail.com>
2019-10-01 13:12:27 -0500

    Updating docstring again

--- Module6/src/string_functions.py
+++ Module6/src/string_functions.py
@@ -4,7 +4,10 @@ Author: Wes Brown
 Last date modified: 10/01/19
 
 Purpose: Functions related to string and manipulating them.
+"""
 
+"""
+This repeats an entered message a number of times.
 :param message: this is the message that will be repeated
 :param n: this is the number of times the message will be repeated
 :returns multiple_message: the message repeated n number of times
