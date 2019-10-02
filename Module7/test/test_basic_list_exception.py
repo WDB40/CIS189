aa2c51ab8a8bdcb785806a68895f0b1b7f5f3627
WDB40 <wbrown1640@gmail.com>
2019-10-01 19:13:15 -0500

    Creating the failing tests.

--- Module7/test/test_basic_list_exception.py
+++ Module7/test/test_basic_list_exception.py
@@ -9,7 +9,7 @@ Purpose: Test working with exceptions with lists in python
 
 import unittest
 from unittest.mock import patch
-from Module7.src.basic_list import *
+from Module7.src.basic_list_exception import *
 
 
 class MyTestCase(unittest.TestCase):
@@ -24,6 +24,22 @@ class MyTestCase(unittest.TestCase):
     @patch('Module7.src.basic_list_exception.get_input', return_value=20)
     def test_make_list_3(self, input):
         self.assertEqual(make_list(), [20, 20, 20])
+
+    @patch('Module7.src.basic_list_exception.get_input', return_value='Wes')
+    def test_make_list_non_numeric(self, input):
+        with self.assertRaises(ValueError):
+            make_list()
+
+    @patch('Module7.src.basic_list_exception.get_input', return_value=55)
+    def test_make_list_above_range(self, input):
+        with self.assertRaises(ValueError):
+            make_list()
+
+    @patch('Module7.src.basic_list_exception.get_input', return_value=0)
+    def test_make_list_below_range(self, input):
+        with self.assertRaises(ValueError):
+            make_list()
+
 
 
 if __name__ == '__main__':
