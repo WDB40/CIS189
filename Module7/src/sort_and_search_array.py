bdbc47b2046a3865814902f6e65d2e80c6ebd845
WDB40 <wbrown1640@gmail.com>
2019-10-07 22:21:13 -0500

    Set up the files and tests for search_array()

---
+++ Module7/src/sort_and_search_array.py
@@ -0,0 +1,16 @@
+"""
+Program: sort_and_search_array.py
+Author: Wes Brown
+Last date modified: 10/07/19
+
+Purpose: Sorting and searching an array in python.
+"""
+
+
+def sort_array(items):
+    # from a testability perspective, I am going to return a value
+    pass
+
+
+def search_array(items, item):
+    pass
---
+++ Module7/test/test_sort_and_search_array.py
@@ -0,0 +1,23 @@
+"""
+Program: sort_and_search_array.py
+Author: Wes Brown
+Last date modified: 10/07/19
+
+Purpose: Sorting and searching an array in python.
+"""
+
+
+import unittest
+from Module7.src.sort_and_search_array import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_search_item_found(self):
+        self.assertEqual(search_array([1, 2, 3, 4, 5], 3), 2)
+
+    def test_search_item_not_found(self):
+        self.assertEqual(search_array([1, 2, 3, 4, 5], 6), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
