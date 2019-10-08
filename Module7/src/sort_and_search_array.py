06e0b09ffa1f14c5db382b5c9785a120f230ac29
WDB40 <wbrown1640@gmail.com>
2019-10-07 22:25:57 -0500

    Completing search_array()

--- Module7/src/sort_and_search_array.py
+++ Module7/src/sort_and_search_array.py
@@ -7,10 +7,26 @@ Purpose: Sorting and searching an array in python.
 """
 
 
+import array as arr
+
+
 def sort_array(items):
     # from a testability perspective, I am going to return a value
     pass
 
 
 def search_array(items, item):
+    items_array = arr.array('i', items)
+    INVALID_VALUE = -1
+    index = INVALID_VALUE
+
+    try:
+        index = items_array.index(item)
+    except ValueError:
+        index = INVALID_VALUE
+
+    return index
+
+
+if __name__ == '__main__':
     pass
