387a9834521ea0770ac4c400747f139eb0a3f621
WDB40 <wbrown1640@gmail.com>
2019-10-07 20:47:38 -0500

    Setup and tests for search_list

---
+++ Module7/src/sort_and_search_list.py
@@ -0,0 +1,19 @@
+"""
+Program: sort_and_search_list.py
+Author: Wes Brown
+Last date modified: 10/07/19
+
+Purpose: Sorting and searching a list in python.
+"""
+
+
+def sort_list():
+    pass
+
+
+def search_list(list, value):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ Module7/test/test_sort_and_search_list.py
@@ -0,0 +1,23 @@
+"""
+Program: test_sort_and_search_list.py (sort_and_search_list.py)
+Author: Wes Brown
+Last date modified: 10/07/19
+
+Purpose: Testing the sorting and searching a list in python.
+"""
+
+
+import unittest
+from Module7.src.sort_and_search_list import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_search_item_found(self):
+        self.assertEqual(search_list([1, 2, 3, 4], 3), 2)
+
+    def test_search_item_not_found(self):
+        self.assertEqual(search_list([1, 2, 3, 4], 5), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
