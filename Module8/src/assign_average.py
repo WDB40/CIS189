fe9809621a28323c1ac5bd0c51db824117582d5d
WDB40 <wbrown1640@gmail.com>
2019-10-15 22:58:04 -0400

    Adding tests for switch_average()

---
+++ Module8/src/assign_average.py
@@ -0,0 +1,11 @@
+"""
+Program: assign_average.py
+Author: Wes Brown
+Last date modified: 10/15/19
+
+Purpose: Using dictionaries to implement a switch-esque statement
+"""
+
+
+def switch_average(key):
+    pass
---
+++ Module8/test/test_assign_average.py
@@ -0,0 +1,39 @@
+"""
+Program: test_assign_average.py (assign_average.py)
+Author: Wes Brown
+Last date modified: 10/15/19
+
+Purpose: Test using dictionaries to implement a switch-esque statement
+"""
+
+
+import unittest
+from Module8.src.assign_average import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_return_A(self):
+        self.assertEqual(switch_average("A"), 90)
+
+    def test_return_a(self):
+        self.assertEqual(switch_average("a"), 90)
+
+    def test_return_B(self):
+        self.assertEqual(switch_average("B"), 80)
+
+    def test_return_C(self):
+        self.assertEqual(switch_average("C"), 70)
+
+    def test_return_D(self):
+        self.assertEqual(switch_average("D"), 60)
+
+    def test_return_F(self):
+        self.assertEqual(switch_average("F"), 50)
+
+    def test_key_not_found(self):
+        with self.assertRaises(KeyError):
+            switch_average("G")
+
+
+if __name__ == '__main__':
+    unittest.main()
