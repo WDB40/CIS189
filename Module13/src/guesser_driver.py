012ad9ba9b080e20d42ec270805e70721ba5e240
WDB40 <wbrown1640@gmail.com>
2019-11-19 19:53:44 -0600

    Completing the game.

--- Module13/src/guesser_driver.py
+++ Module13/src/guesser_driver.py
@@ -9,6 +9,7 @@ Purpose: Driving the number guesser game
 import tkinter
 from tkinter import messagebox
 from random import randint
+from Module13.src.number_guesser import NumberGuesser
 
 
 def select_number(value):
@@ -16,17 +17,28 @@ def select_number(value):
     if value == winning_number:
         messagebox.showinfo("Winner!", f"You selected the correct number: {value}")
         reset_game()
+    else:
+        global guesses
+        guesses.add_guess(value)
 
 
 def reset_game():
     for button in buttons.values():
         button.config(state="normal")
+    create_new_winning_number()
+    global guesses
+    guesses = NumberGuesser()
+
+
+def create_new_winning_number():
     global winning_number
     winning_number = randint(0, 10)
 
 
 if __name__ == '__main__':
-    winning_number = randint(0, 10)
+    winning_number = 0
+    create_new_winning_number()
+    guesses = NumberGuesser()
 
     buttons = {}
 
@@ -40,5 +52,7 @@ if __name__ == '__main__':
         buttons[i] = tkinter.Button(game, text=f"{i}")
         buttons[i].config(command=lambda value=i: select_number(value))
         buttons[i].grid(row=i)
+
+    close_button = tkinter.Button(game, text="Close Game", command=game.destroy).grid(row=11)
 
     game.mainloop()
