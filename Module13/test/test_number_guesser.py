a7c3e50d35091283e95381e47a861620b08aa0f3
WDB40 <wbrown1640@gmail.com>
2019-11-19 18:30:01 -0600

    Add testing for preventing duplicate additions

--- Module13/test/test_number_guesser.py
+++ Module13/test/test_number_guesser.py
@@ -25,6 +25,15 @@ class MyTestCase(unittest.TestCase):
         if value not in self.number_guesser.guessed_list:
             self.fail("Value not added to guessed list.")
 
+    def test_prevent_duplicate_additions(self):
+        value = 7
+        expected = 1
+
+        self.number_guesser.add_guess(value)
+        self.number_guesser.add_guess(value)
+
+        self.assertEqual(expected, len(self.number_guesser.guessed_list))
+
 
 if __name__ == '__main__':
     unittest.main()
